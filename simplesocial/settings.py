1d1d1d4a0fbc793e44f433582959664ede8b0949
harish <harishthakur4455@gmail.com>
2021-06-07 20:08:46 +0530

    commit
    commit

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -124,14 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = ''
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 LOGIN_REDIRECT_URL = 'test'
