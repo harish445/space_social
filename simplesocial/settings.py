dfacd1e110a8bff42473d5cb6a338203c4e3c045
harish <harishthakur4455@gmail.com>
2021-06-07 20:01:37 +0530

    fianl commmit
    debug settings.py

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kb&*9vfx0m$#gw$t!l&f#gdmume0cg!90s(emmpuxq8jhpvq$m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['space-social.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','space-social.herokuapp.com']
 
 
 # Application definition
@@ -125,9 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+if not DEBUG:
+    STATIC_ROOT = ''
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 
 LOGIN_REDIRECT_URL = 'test'
